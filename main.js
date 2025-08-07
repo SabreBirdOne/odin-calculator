b94dc268e76f2dd932fb026c44889cf59c5249fc
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-08-07 16:35:02 -0400

    Add listener to digit buttons in index.html
    The listener just concatenates the digit char to
    the display's text content.

--- main.js
+++ main.js
@@ -21,3 +21,12 @@ function operate(fn, a, b){
 let numberA = 0;
 let numberB = 0;
 let operator = null;
+
+const display = document.querySelector("#display"); 
+
+const numberButtons = [...document.querySelectorAll(".numberButton")];
+numberButtons.sort((buttonA, buttonB) => buttonA.id.charAt(-1) - buttonB.id.charAt(-1));
+
+for(let i = 0; i < numberButtons.length; i++){
+    numberButtons[i].addEventListener("click", () => {display.textContent += `${i}`});
+}
